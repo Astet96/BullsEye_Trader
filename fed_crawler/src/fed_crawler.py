31d15207fa1515e81a6561abaa9f98974c543c64
Alexandru <alexandru@suade.org>
2024-07-26 15:07:33 +0100

    ci: adds ci testing

---
+++ fed_crawler/src/fed_crawler.py
@@ -0,0 +1,5 @@
+"""This module is a webcrawler that aggregates congresional trading disclosures"""
+
+def mock_fn():
+    """This is a mock function"""
+    return True
---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup  # noqa: E0401
+
+if __name__ == "__main__":
+    setup()
