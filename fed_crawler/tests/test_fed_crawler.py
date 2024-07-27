0a3493a74ae834529f602dacb4c10121fb46ad84
Alexandru <alexandru@suade.org>
2024-07-27 16:33:02 +0100

    ci: update testing pipeline to split sub-modules & main project into separate workers

--- fed_crawler/tests/test_fed_crawler.py
+++ fed_crawler/tests/test_fed_crawler.py
@@ -1,6 +1,4 @@
-"""Testing for the fed_crawler module"""
-
-from fed_crawler.src import mock_fn
+from ..src import mock_fn
 
 def test_mock():
     """This is a mock test"""
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 # from fastapi import FastAPI
+
+def mock_fn2():
+    """This is a mock function"""
+    return True
